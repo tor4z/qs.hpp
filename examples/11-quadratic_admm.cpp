f590b5b2fd396c9c839749c8755fd0b935262224
chenwj <chenwj@raincom.cn>
2025-09-11 17:52:44 +0800

    Verify admm algo

--- examples/11-quadratic_admm.cpp
+++ examples/11-quadratic_admm.cpp
@@ -1,8 +1,6 @@
 #include "qs.hpp"
+#include <cstdlib>
 #include <iostream>
-#include <thread>
-
-using namespace std::chrono_literals;
 
 template<typename T>
 T soft_thresholding(T x, float lambda, float tau)
@@ -17,11 +15,11 @@ int main()
     qs::Matrixf<3, 3> A;
     qs::Vectorf<3> x;
     qs::Vectorf<3> b;
-    A << 8, 2, 3,
-         2, 9, 5,
-         3, 5, 6;
-    x << 4.0, 6.0, 9.0;
-    b << 3.0, 10.0, 5.0;
+    A << 1, 1, -1,
+         4, 2, 1,
+         1, -2, -2;
+    x << 4.0, 6.0, 9.0; // initial
+    b << 0, 7, -9;
 
     std::cout << "A: " << A << "\n";
     std::cout << "b: " << b << "\n";
@@ -36,16 +34,47 @@ int main()
     auto z = x.rand();
     auto y = x.rand();
 
+    auto last_fx{(A * x - b).norm2() + lambda * x.norm1()};
     while (1) {
         x = AtA_tauI_inv * (Atb + tau_inv * (z - y));
         z = soft_thresholding(x + y, lambda, 1.0 / tau_inv);
         y = y + tau_inv * (x - z);
 
         auto fx{(A * x - b).norm2() + lambda * x.norm1()};
-        std::cout << "fx: " << fx << "\n";
-        std::cout << "x: " << x << "\n";
-        std::this_thread::sleep_for(100ms);
+        if (std::abs(fx - last_fx) < 1.0e-6) {
+            break;
+        }
+        last_fx = fx;
     }
+
+    std::cout << "fx: " << last_fx << "\n";
+    std::cout << "result : " << x << "\n";
 
     return 0;
 }
+
+/* python code for verify
+
+import numpy as np
+
+# Define the 3x3 coefficient matrix A
+A = np.array([
+    [1, 1, -1],
+    [4, 2, 1],
+    [1, -2, -2]
+])
+
+# Define the constants vector b
+b = np.array([0, 7, -9])
+
+# Solve for x
+x = np.linalg.solve(A, b)
+
+print("Solution vector x:", x)
+# Expected output: Solution vector x: [1. 2. 3.]
+
+# Verify the solution
+print("Verification (A @ x):", A @ x)
+# Expected output: Verification (A @ x): [0. 7. -9.]
+
+*/
